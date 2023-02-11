ca2e094c666b7cfebef551ba4ceef434f8e19af0
izzanka <izzancoc.46@gmail.com>
2023-02-11 22:34:58 +0700

    create routes for auth, link and home

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\User\AuthController;
+use App\Http\Controllers\User\LinkController;
 use App\Http\Livewire\User\Auth\SignIn;
 use App\Http\Livewire\User\Auth\SignUp;
 use Illuminate\Support\Facades\Route;
@@ -20,9 +21,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/sign-up', SignUp::class)->name('sign-up');
-Route::get('/sign-in', SignIn::class)->name('sign-in');
+Route::get('/sign-in', SignIn::class)->name('login');
 
 Route::middleware(['auth'])->group(function () {
+
+    Route::get('/links', [LinkController::class, 'index'])->name('links');
+
     Route::post('/sign-out', [AuthController::class, 'signout'])->name('sign-out');
 });
 
