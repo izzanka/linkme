140ff134770beb83a19f91a6ff9f0d0604d1c55e
izzanka <izzancoc.46@gmail.com>
2023-02-14 09:03:54 +0700

    add profile update function

--- app/Http/Livewire/User/Profile/ProfileIndex.php
+++ app/Http/Livewire/User/Profile/ProfileIndex.php
@@ -2,11 +2,45 @@
 
 namespace App\Http\Livewire\User\Profile;
 
+use App\Http\Requests\User\Profile\UpdateProfileRequest;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ProfileIndex extends Component
 {
-    public $username, $credential;
+    use WithFileUploads;
+
+    public $username, $credential, $image;
+
+    public function rules()
+    {
+        return (new UpdateProfileRequest)->rules();
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+
+        auth()->user()->update([
+            'username' => $this->username,
+            'credential' => $this->credential,
+        ]);
+
+        $this->emit('link-preview-render');
+    }
+
+    public function updateImage()
+    {
+        $this->validate([
+            'image' => 'required|max:2048',
+        ]);
+
+        auth()->user()->media()->delete();
+
+        auth()->user()->addMediaFromDisk('livewire-tmp/' . $this->image->getFileName())->toMediaCollection('user');
+
+        $this->emit('link-preview-render');
+    }
 
     public function render()
     {
--- resources/views/livewire/user/profile/profile-index.blade.php
+++ resources/views/livewire/user/profile/profile-index.blade.php
@@ -1,24 +1,53 @@
 <div>
-    <h2><b>Profile</b></h2>
+    <h2><b>Profile</b>
+        <div wire:loading wire:target="username, credential">
+            <span class="spinner-border spinner-border-sm ml-2" role="status"></span>
+        </div>
+    </h2>
+
     <div class="card rounded-4 mt-2">
         <div class="card-body">
-            <div class="row">
-                <div class="col-3 mt-2">
-                    <span class="border border-dark avatar avatar-xl rounded-circle" style="background-image: url({{ auth()->user()->getFirstMediaUrl('user','thumb') }})"></span>
-                </div>
-                <div class="col-9 mt-2">
-                    <div class="d-grid gap-2">
-                        <button class="btn btn-purple rounded-4" type="button">Pick an image</button>
-                        <button disabled class="btn btn-secondary rounded-4" type="button">Remove</button>
+            <form wire:submit.prevent="updateImage">
+                <div class="row">
+                    <div class="col-3 mt-2">
+                        <span class="border border-dark avatar avatar-xl rounded-circle" style="background-image: url({{ auth()->user()->getFirstMediaUrl('user','thumb') }})"></span>
+                    </div>
+                    <div class="col-9 mt-2">
+                        <div wire:loading wire:target="image">
+                            <div class="mb-3">
+                                <div class="spinner-border spinner-border-sm" role="status"></div>
+                                Uploading image...
+                            </div>
+                        </div>
+                        <input type="file" class="form-control rounded-4 @error('image') is-invalid @enderror" wire:model.lazy="image">
+                            @error('image')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        <div class="d-grid gap-2 mt-2">
+                            <button class="btn btn-purple rounded-4" type="submit">Update</button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </form>
+
             <div class="row mt-4 mb-3">
                 <div class="row">
-                    <input type="text" class="form-control rounded-3" wire:model="username">
+                    <input type="text" class="form-control rounded-3 @error('username') is-invalid @enderror" wire:model.lazy="username">
+                    @error('username')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 <div class="row mt-2">
-                    <input type="text" class="form-control rounded-3" wire:model="credential">
+                    <input type="text" class="form-control rounded-3 @error('credential') is-invalid @enderror" wire:model.lazy="credential">
+                    @error('credential')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
             </div>
         </div>
