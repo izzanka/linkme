cc4d0019d85404b729818bcc88e5fd48024e378c
izzanka <izzancoc.46@gmail.com>
2023-08-30 08:42:43 +0700

    edit factory & seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,12 +17,17 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $username = fake()->firstName();
+
         return [
-            'name' => fake()->name(),
+            'username' => $username,
+            'username_slug' => Str::slug($username),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'image' => 'https://ui-avatars.com/api/?name=' . $username . '&background=random&rounded=true&size=112',
+            'bio' => fake()->sentence(3),
         ];
     }
 
